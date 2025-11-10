d2a619f63cfa41a664281cb02696b426f77f08c7
Jonas Barbosa <119669103+jonas9200@users.noreply.github.com>
2025-11-10 13:11:39 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Configuração padrão para Vite + React
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    host: true
   }
-})
+});
